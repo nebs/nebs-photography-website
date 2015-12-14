0c3cd94bc806522e7fdcc08dba07200d0be184b9
Nebojsa Petrovic <nebspetrovic@gmail.com>
2015-12-13 22:48:35 -0800

    Add street photographs and slim down portraits

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 // TO ADD NEW PHOTOS, UPDATE THIS DICTIONARY.
 // Note: Gallery names must correspond to folders directly.
+// Minify with: http://refresh-sf.com/
 var galleries = {
-	"People I": ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg"],
-	"People II": ["9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg"],
+	"Portraits": ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"],
+	"Street": ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"],
 };
 
 var currentGalleryName = "";
