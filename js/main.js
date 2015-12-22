8fba2506314c83340f779c5a5c2633afa1b10a3f
Nebojsa Petrovic <nebspetrovic@gmail.com>
2015-12-22 10:21:56 -0800

    Add 8 new street photos

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
 // Note: Gallery names must correspond to folders directly.
 // Minify with: http://refresh-sf.com/
 var galleries = {
-	"Street": ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg"],	
+	"Street": ["1.jpg", "2.jpg",  "3.jpg",  "4.jpg",  "5.jpg",  "6.jpg",  "7.jpg",  "8.jpg", 
+	           "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg", "16.jpg"],	
 	"Portraits": ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"],
 };
 
